b1037ffad8fda640e25bfa13488a05a2aac5e0d3
Andrea Kramm <krammandrea@gmail.com>
2011-09-27 23:04:49 -0700

    added image map, incomplete

--- Mandelbrotserver.py
+++ Mandelbrotserver.py
@@ -18,6 +18,8 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	    self.do_GET_main_page()
 	elif self.path.endswith("Mandelbrot.png"):
 	    self.do_GET_Mandelbrot()
+	elif '?x'in self.path:
+	    self.do_GET_new_coordinate()
 	else:
 	    self.send_response(404)
 
@@ -28,10 +30,11 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write("<html><head><title>Mandelbrot</title></head>")
         self.wfile.write("<body><p>This is a test.</p>")
-	self.wfile.write("<img src='Mandelbrot.png'>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
-        self.wfile.write("<p>You accessed path: %s</p>" % self.path)
+	#self.wfile.write("<img src='Mandelbrot.png'>")
+	self.wfile.write("<form action='/' method=get><input type=image src='/Mandelbrot.png' alt='click for zoom' border=0></form>")        
+# If someone went to "http://something.somewhere.net/foo/bar/",
+	 # then s.path equals "/foo/bar/".
+        #self.wfile.write("<p>You accessed path: %s</p>" % self.path)
         self.wfile.write("</body></html>")
 	print self.path
 
@@ -43,6 +46,18 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	pngfile = open("Mandelbrot.png","rb")
 	self.wfile.write(pngfile.read())
 	pngfile.close()
+
+    #handles http://localhost:8080/?x=658&y=586 requests
+    def do_GET_new_coordinate():
+	#extract new coordinates
+	    # find the block of numbers after 'x=' and 'y='
+	regExp = re.compile(r"x=([0-9]+)")
+	new_x= atoi(regExp.findall(self.path()))
+	regExp = re.compile(r"y=([0-9]+)")
+        new_y = atoi(regExp.findall(self.path())) 
+	self.end_headers()
+	#TODO calculate_mandelbrot()	
+	self.do_GET_main_page()
     
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
