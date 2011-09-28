4ea258623af1fd8ad9f4c79b13f4f003ea447abb
Andrea Kramm <krammandrea@gmail.com>
2011-09-28 13:53:22 -0700

    eliminated the rest of the bugs of the new introduced fuctionality

--- Mandelbrotserver.py
+++ Mandelbrotserver.py
@@ -29,15 +29,20 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write("<html><head><title>Mandelbrot</title></head>")
+	self.show_main_page()
+
+    def show_main_page(self):
+	self.wfile.write("<html><head><title>Mandelbrot</title></head>")
         self.wfile.write("<body><p>This is a test.</p>")
-	#self.wfile.write("<img src='Mandelbrot.png'>")
-	self.wfile.write("<form action='/' method=get><input type=image src='/Mandelbrot.png' alt='click for zoom' border=0></form>")        
-# If someone went to "http://something.somewhere.net/foo/bar/",
-	 # then s.path equals "/foo/bar/".
+        #self.wfile.write("<img src='Mandelbrot.png'>")
+        self.wfile.write("<form action='/' method=get><input type=image src='/Mandelbrot.png' alt='click for zoom' border=0></form>")
+	# If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
         #self.wfile.write("<p>You accessed path: %s</p>" % self.path)
         self.wfile.write("</body></html>")
-	print self.path
+        print self.path
+
+
 
     def do_GET_Mandelbrot(self):
 
@@ -53,13 +58,13 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 	#extract new coordinates
 	    # find the block of numbers after 'x=' and 'y='
 	regExp = re.compile(r"x=([0-9]+)")
-	new_x= string.atoi(regExp.findall(self.path))
+	new_x= string.atoi(regExp.findall(self.path)[0])
 	regExp = re.compile(r"y=([0-9]+)")
-        new_y = string.atoi(regExp.findall(self.path)) 
+        new_y = string.atoi(regExp.findall(self.path)[0]) 
 	self.end_headers()
 	#TODO calculate_mandelbrot()	
-	self.do_GET_main_page()
-    
+	self.show_main_page()   
+ 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
