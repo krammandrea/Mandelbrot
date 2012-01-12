0aebe26ac0898cc5262bef42f38e6d2408016c66
Andrea Kramm <krammandrea@gmail.com>
2012-01-11 21:21:26 -0800

    restructured Mandelbrotserver, started to add ImageAdministrator class

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,6 +1,6 @@
 import Image, ImageDraw
 
-def calculate_mandelbrot(imageheight=400,imagewidth=400,maxiteration=10,offsetx=200,offsety=200,zoomfactor=2,colorscheme=[(0,0,0),(51,102,51),(51,102,77),(51,102,102),(51,77,102),(51,51,102),(77,51,102),(102,51,102),(102,51,77),(102,51,51),(102,77,51),(102,102,51),(77,102,51)]):
+def calculate_mandelbrot(imageheight=400,imagewidth=400,maxiteration=10,offsetx=0,offsety=0,zoomfactor=1,colorscheme=[(0,0,0),(51,102,51),(51,102,77),(51,102,102),(51,77,102),(51,51,102),(77,51,102),(102,51,102),(102,51,77),(102,51,51),(102,77,51),(102,102,51),(77,102,51)]):
         #imageheight,imagewidth: pixelsize of the image
         #maxiteration: directly correlated to the duration of the calculation ?when does it get too long
         #offsetx, offsety: new center of the reference image
@@ -22,7 +22,7 @@ def calculate_mandelbrot(imageheight=400,imagewidth=400,maxiteration=10,offsetx=
     for x in xCoord:
         for y in yCoord:
 	   iteration =0
-    	   z0 = complex((float(x)-imagewidth/2-offsetx)/imagewidth/zoomfactor,(float(y)-imageheight/2-offsety)/imageheight/zoomfactor)
+    	   z0 = complex(4*(float(x)-imagewidth/2-offsetx)/imagewidth/zoomfactor,4*(float(y)-imageheight/2-offsety)/imageheight/zoomfactor)
     	   z = complex(0,0)	#z=0+0*j
     	   #apply equation
     	   while (iteration < maxiteration and  abs(z*z.conjugate()) < 2**2):
--- Mandelbrotserver.py
+++ Mandelbrotserver.py
@@ -6,42 +6,68 @@ import Mandelbrot
 HOST_NAME = '' # empty because using http://localhost
 PORT_NUMBER = 8080
 
+class ImageAdministrator():
+    def __init__(self):
+	self.height = 1000
+	self.width = 1000
+	self.maxiteration = 10
+	self.offsetx = self.height/2
+	self.offsety = self.width/2
+	self.zoomfactor = 1
+	self.colorscheme = [(0,0,0),(51,102,51),(51,102,77),(51,102,102),(51,77,102),(51,51,102),(77,51,102),(102,51,102),(102,51,77),(102,51,51),(102,77,51),(102,102,51),(77,102,51)] #GREEN
 
+    def reset_to_default(self):
+	pass
+    def change_offset_and_zoom(self, new_x, new_y):
+	self.offsetx 
+    def change_zoom(self):
+	pass
+    def change_offset(self):
+	pass
+    def change_colorscheme(self):
+	pass
+    def change_maxiteration(self):
+	pass
+    def get_parameters(self):
+	return self.height, self.width, self.maxiteration, self.offsetx, self.offsety, self.zoomfactor, self.colorscheme
+    def save_parameters_to_file(self):
+	pass
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
     def do_GET(self):
 	if self.path.endswith("index.html"):
 	    self.do_GET_main_page()
 	elif self.path.endswith("/"):
-	    self.do_GET_main_page()
+	    self.get_main_page()
 	#TODO only if ?x AND zoom_offset
 	elif '?x'in self.path:
-	    self.do_GET_new_coordinate()
+	    new_x,new_y = self.get_new_coordinate()
+
+	    #calculate absolute zoomfactor and offset from all the relative factors in the recent browsing history
+	    Mandelbrot.calculate_mandelbrot()	
+	    #imageheight,imagewidth: pixelsize of the image
+            #maxiteration: directly correlated to the duration of the calculation ?when does it get too long
+            #offsetx, offsety: new center of the reference image
+            #zoomfactor: absolute zoomfactor range 1 to indefinitely
+            #colorscheme: 12 elements, default value is green	
+	    self.get_main_page()   
 	#TODO /zoom_in /zoom_out and /zoom
 	elif self.path.find("/MandelbrotImg/") >=0:
 	    #extract the name of the requested picture
 	    #do_GET_image("Imagename")
-	    self.do_GET_image(self.path.rpartition("MandelbrotImg/")[-1])
+	    self.get_image(self.path.rpartition("MandelbrotImg/")[-1])
 	    
 	else:
 	    self.send_response(404)
 #TODO self.send_header("Content-type","application/x-download")
-    def do_GET_main_page(self):
+    def get_main_page(self):
         """Respond to a GET request."""
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-	self.show_main_page()
-	
-    def show_main_page(self):
-
 	main_page_html = open("Mandelbrotserver.html","r")
 	self.wfile.write(main_page_html.read())
 
-    def do_GET_image(self,imagename):
+    def get_image(self,imagename):
 	
 	if imagename.endswith(".png"):
 	    self.send_response(200)
@@ -57,17 +83,14 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     #handles http://localhost:8080/?x=658&y=586 requests
     #TODO should handle http://localhost:8080/zoom_offset?x=658&y=586 requests
 
-    def do_GET_new_coordinate(self):
+    def get_new_coordinate(self):
 	#extract new coordinates
 	    # find the block of numbers after 'x=' and 'y='
 	regExp = re.compile(r"x=([0-9]+)")
 	new_x= string.atoi(regExp.findall(self.path)[0])
 	regExp = re.compile(r"y=([0-9]+)")
         new_y = string.atoi(regExp.findall(self.path)[0]) 
-	#calculate absolute zoomfactor and offset from all the relative factors in the recent browsing history
-	Mandelbrot.calculate_mandelbrot()	
-	
-	self.do_GET_main_page()   
+	return new_x, new_y
  
 	
 	
