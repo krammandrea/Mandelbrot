ac351cc009e61599b83d770754a571eb07a6d8e1
Andrea Kramm <krammandrea@gmail.com>
2012-03-26 21:24:36 -0700

    added resize, choose color, choose iteration functionality

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import string
 import re
 import BaseHTTPServer
 import mandelbrot
+import math
 
 HOST_NAME = '' # empty because using http://localhost
 PORT_NUMBER = 8080
@@ -11,26 +12,37 @@ PORT_NUMBER = 8080
 #TODO: put class in new file, find name, find the big picture, comment pydoc, comment variables, is it a module?
 class ImageAdministrator():
     """stores the current parameters of the image which the user changes until he is satisfied and saves the image and the accompaning calculating data to file""" 
-    GREEN= [(0,0,0),(51,102,51),(51,102,77),(51,102,102),(51,77,102),(51,51,102),(77,51,102),(102,51,102),(102,51,77),(102,51,51),(102,77,51),(102,102,51),(77,102,51)]
-    #"""default colorscheme in green"""
-
-
+    GREEN =["#000000","#336633","#33664D","#336666","#334D66","#333366","#4D3366","#663366","#66334D","#663333","#664D33","#666633","#4D6633"]
+    BROWNBLUE = ["#000000","#FF9900","#BF8630","#A66300","#FFB240","#FFC773","#689CD2","#4188D2","#04376C","#26517C","#0D58A6"]
+    #"""default colorscheme in brown and blue"""
     def __init__(self):
 	self.height = 600
 	self.width = 600
-	self.maxiteration = 10
+	self.maxiteration = 20
         self.xabsolutestart = -2.0
         self.xabsoluteend = 2.0
         self.yabsolutestart = -2.0
         self.yabsoluteend = 2.0
-	self.colorscheme = self.GREEN	
+	self.colorscheme = self.BROWNBLUE	
 
 
     def change_imagesize(self,new_width, new_height):
 #TODO remember to adjust zoomfactor if zoom=1 and image not quadratic
         """adjust dependent offsetabsolute"""
-        pass
-
+        #adjust the new x/yabsolutestart/end to the new aspectratio, so the detail of the image has still the same surface and is centered around the old centerpoint
+        new_ratio = float(new_height)/float(new_width)
+        surface = (self.xabsoluteend-self.xabsolutestart)*(self.yabsoluteend-self.yabsolutestart)
+        new_xabsolutewidth = math.sqrt(surface/new_ratio)
+        new_yabsoluteheight = math.sqrt(surface*new_ratio)
+        width_difference = new_xabsolutewidth-(self.xabsoluteend-self.xabsolutestart)
+        height_difference = new_yabsoluteheight-(self.yabsoluteend-self.yabsolutestart)
+        self.xabsolutestart -= 0.5*width_difference
+        self.xabsoluteend   += 0.5*width_difference
+        self.yabsolutestart -= 0.5*height_difference
+        self.yabsoluteend   += 0.5*height_difference
+        self.height = new_height
+        self.width  = new_width
+#TODO
 
     def reset_to_default(self):
 	pass
@@ -41,11 +53,12 @@ class ImageAdministrator():
         #zoom = 2 halfs the section of the image 
         newimagewidth   = (self.xabsoluteend-self.xabsolutestart)/zoom_relative
         newimageheight  = (self.yabsoluteend-self.yabsolutestart)/zoom_relative 
-
-        self.xabsolutestart += 0.5*(self.xabsoluteend-self.xabsolutestart-newimagewidth) 
-        self.yabsolutestart += 0.5*(self.yabsoluteend-self.yabsolutestart-newimageheight)
-        self.xabsoluteend   -= 0.5*(self.xabsoluteend-self.xabsolutestart-newimagewidth)
-        self.yabsoluteend   -= 0.5*(self.yabsoluteend-self.yabsolutestart-newimageheight)     
+        oldimagewidth   = (self.xabsoluteend-self.xabsolutestart)
+        oldimageheight  = (self.yabsoluteend-self.yabsolutestart)
+        self.xabsolutestart += 0.5*(oldimagewidth-newimagewidth) 
+        self.xabsoluteend   -= 0.5*(oldimagewidth-newimagewidth)
+        self.yabsolutestart += 0.5*(oldimageheight-newimageheight)
+        self.yabsoluteend   -= 0.5*(oldimageheight-newimageheight)     
 
     def change_offset(self, xoffsetfactor, yoffsetfactor):
         xabsoluteoffset = xoffsetfactor*(self.xabsoluteend - self.xabsolutestart) 
@@ -71,12 +84,12 @@ class ImageAdministrator():
         self.change_zoom(zoom_on_click)
 
 
-    def change_colorscheme(self):
-	pass
+    def change_colorscheme(self,new_colorscheme):
+        self.colorscheme = new_colorscheme
 
 
-    def change_maxiteration(self):
-	pass
+    def change_maxiteration(self,new_iteration):
+	self.maxiteration = new_iteration
 
     def get_parameters(self):
 	return self.height, self.width, self.maxiteration, self.xabsolutestart, self.xabsoluteend, self.yabsolutestart, self.yabsoluteend, self.colorscheme
@@ -99,16 +112,31 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #TODO hash table for url path instead of if elif
     def do_GET(self):
 	if self.path.endswith("index.html"):
-	    mandelbrot.calculate_mandelbrot()
+	    mandelbrot.calculate_mandelbrot(*self.imageAdministrator.get_parameters())	
 	    self.do_GET_main_page()
 	elif self.path.endswith("/"):
-	    mandelbrot.calculate_mandelbrot()
+	    mandelbrot.calculate_mandelbrot(*self.imageAdministrator.get_parameters())	
 	    self.get_main_page()
 	elif self.path.find("/images/") >=0:
             pathname = self.path.partition("images/")[1]+self.path.partition("images/")[2]
             self.get_image(pathname)
         elif self.path.find("/style/")>=0:
             self.get_css(self.path.rpartition("style/")[-1])
+        elif self.path.find("change_color") >=0:
+            new_colors = self.get_colors(self.path.partition("change_color")[2])
+            self.imageAdministrator.change_colorscheme(new_colors)
+	    mandelbrot.calculate_mandelbrot(*self.imageAdministrator.get_parameters())	
+            self.get_main_page()
+        elif self.path.find("iteration")>=0:
+            new_iteration = self.get_iteration(self.path.partition("iteration")[2])
+            self.imageAdministrator.change_maxiteration(new_iteration)
+	    mandelbrot.calculate_mandelbrot(*self.imageAdministrator.get_parameters())	
+            self.get_main_page()
+        elif self.path.find("change_size")>=0:
+            new_width,new_height = self.get_size(self.path.partition("change_size")[2])
+            self.imageAdministrator.change_imagesize(new_width,new_height)
+	    mandelbrot.calculate_mandelbrot(*self.imageAdministrator.get_parameters())	
+            self.get_main_page()
         elif "/javascript"in self.path and ".js"in self.path:
             pathname = self.path.partition("javascript/")[1]+self.path.partition("javascript/")[2]
             self.get_jscolor(pathname)
@@ -152,7 +180,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.get_main_page() 
 	elif self.path.find("save") >=0:
             self.download_fractal_param_dat()
-               
+
 	else:
 	    self.send_response(404)
 
@@ -177,6 +205,14 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         else:
             self.send_response(404)    
 
+    def get_size(self,sizestring):
+	regExp = re.compile(r"height=([0-9]+)")
+	new_height= string.atoi(regExp.findall(self.path)[0])
+	regExp = re.compile(r"width=([0-9]+)")
+	new_width= string.atoi(regExp.findall(self.path)[0])
+        return new_width,new_height
+
+
     def get_jscolor(self,jscolorpath):
         if jscolorpath.endswith("jscolor.js"):
             self.send_response(200)
@@ -187,6 +223,23 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             jscolorfile.close()
         else:
             self.send_response(404)        
+
+    def get_colors(self,colorstring):
+        #find all the hex numbers in between "col=" and  "&"
+
+        regExp = re.compile("([0-9a-fA-F]+)(?=&)")
+	new_colors = regExp.findall(self.path)
+        #filter out bad results
+
+        #add "#" in string as required by the ImageDraw library
+        new_colors_formatted = ['#'+ color for color in new_colors]
+        return new_colors_formatted
+    
+    def get_iteration(self,iterationstring):
+        regExp = re.compile("[0-9]{1,2}")
+        new_iteration = string.atoi(regExp.findall(iterationstring)[0]) 
+        print new_iteration
+        return new_iteration
 
 #TODO else only after imagename not existent 
     def get_image(self,imagepathname):
