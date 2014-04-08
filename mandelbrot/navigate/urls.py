520572adc2efa6873fca9e0bb7e3deebe40b1d0d
Andrea Kramm <krammandrea@gmail.com>
2014-04-07 18:34:29 -0700

    Prototype change form to django post

--- mandelbrot/navigate/urls.py
+++ mandelbrot/navigate/urls.py
@@ -3,7 +3,10 @@ from navigate import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^home/(?P<coordinates>\d+)$', views.navigateTo, name='navigateTo'),
     url(r'^home/', views.home, name='home'),
+
+# TODO regex params from url and pass to navigateTo
 
 # ex: /polls/5/results/
 # url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
--- mandelbrot/navigate/views.py
+++ mandelbrot/navigate/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader 
 
+def generatePage(request):
+    """Generic template code"""
 
 # Create your views here.
 def index(request):
     return HttpResponse("Something")
 
 def home(request):
+    """Shows the default image as starting point"""
+    if request.method == "POST":
+        print request.POST
+
+
+    #TODO get previous url and the parameters therein, and init imageAdmin 
+
+    #TODO generate image
+
+
     # a_list = Article.objects.filter(pub_date__year=year) 
     # context = {'year': year, 'article_list': a_list}
     template = loader.get_template('navigate/home.html')
@@ -15,4 +27,17 @@ def home(request):
     return HttpResponse(template.render(context))
     # return render(request, 'navigate/mandelbrotTemplate.html', context)
 
-# def results(request, poll_id):
+def generateImageAt(request):
+    """For a given URL parse and validity check parameters and 
+        generate the appropriate image. Send to home if invalid params."""
+
+def navigateTo(request, coordinates):
+    """Parse the starting point parameters from the url, change them
+        depending on the data from the post request"""
+    print coordinates
+    return home(request)
+    #TODO get previous url and the parameters therein, and init imageAdmin 
+
+    #TODO depending on the post data call imageAdmin and change params
+
+    #TODO redirect to new params
